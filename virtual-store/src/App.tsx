f2caed30d8625e43970968e5fc7bb8d863f4c1b0
Matías E. Moreno <morenomatias847@gmail.com>
2024-02-22 21:21:29 -0300

    Sexto commit - Implementacion del enrutador

--- virtual-store/src/App.tsx
+++ virtual-store/src/App.tsx
@@ -1,12 +1,23 @@
 /* /src/App.tsx */
 import Cart from "./views/Cart.tsx";
-import Home from "./views/Home.tsx"
+import Home from "./views/Home.tsx";
 import Product from "./views/Product.tsx";
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+
+const browserRouter = createBrowserRouter([
+    { path: "/", element: <Home /> },
+    { path: "/cart", element: <Cart /> },
+    { path: "/product/:id", element: <Product/> }
+  ]);
+  
 
 
+/* /src/App.tsx */
 function App() {
-  return <Product/>;
-}
+    return <RouterProvider router={ browserRouter } />
+  }
+  
 export default App;
 
 
